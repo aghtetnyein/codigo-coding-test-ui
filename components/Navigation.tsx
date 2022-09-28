f7315ede9d26c7928d171b46448a97ccde09ab0b
Aung Htet Nyein <aunghtetnyein1999@gmail.com>
2022-09-28 11:26:46 +0630

    Added nav modal

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -16,26 +16,94 @@ import {
 import { navItems } from "../data";
 
 const Navigation = () => {
+  useEffect(() => {
+    const menuBtn = document.getElementById("nav-menu");
+    const navModal = document.getElementById("nav-modal");
+
+    let isOpenModal = false;
+
+    menuBtn?.addEventListener("click", () => {
+      if (!isOpenModal) {
+        isOpenModal = true;
+        navModal!.style.top = "2.8rem";
+        navModal!.style.transform = "translateY(0)";
+        navModal!.style.height = "100vh";
+      } else {
+        isOpenModal = false;
+        navModal!.style.top = "0";
+        navModal!.style.transform = "translateY(-100vh)";
+        navModal!.style.height = "0";
+      }
+    });
+
+    const handleWindowResize = () => {
+      if (window.innerWidth >= 1024) {
+        isOpenModal = false;
+        navModal!.style.top = "0";
+        navModal!.style.transform = "translateY(-100vh)";
+        navModal!.style.height = "0";
+      }
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+  }, []);
+
   return (
-    <ul className={styles.nav}>
-      <Bars2Icon className={`${styles.barsIcon} ${styles.icon}`} />
-      <Link href={"/"}>
-        <Image
-          src="/icons/apple.svg"
-          width={16}
-          height={16}
-          alt="apple-icon"
-          style={{ cursor: "pointer" }}
+    <nav>
+      <ul className={styles.desktopNav}>
+        <Link href={"/"}>
+          <Image
+            src="/icons/apple.svg"
+            width={16}
+            height={16}
+            alt="apple-icon"
+            style={{ cursor: "pointer" }}
+          />
+        </Link>
+
+        {navItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+
+        <MagnifyingGlassIcon
+          className={`${styles.searchIcon} ${styles.icon}`}
         />
-      </Link>
+        <ShoppingBagIcon className="icon" />
+      </ul>
 
-      {navItems.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
+      <ul className={styles.mobileNav}>
+        <Bars2Icon
+          id="nav-menu"
+          className={`${styles.barsIcon} ${styles.icon}`}
+        />
+        <Link href={"/"}>
+          <Image
+            src="/icons/apple.svg"
+            width={16}
+            height={16}
+            alt="apple-icon"
+            style={{ cursor: "pointer" }}
+          />
+        </Link>
+        <ShoppingBagIcon className="icon" />
+      </ul>
 
-      <MagnifyingGlassIcon className={`${styles.searchIcon} ${styles.icon}`} />
-      <ShoppingBagIcon className="icon" />
-    </ul>
+      <div id="nav-modal" className={styles.navModal}>
+        <div
+          style={{
+            padding: "0.5rem 1rem",
+            borderBottom: "1px solid rgba(255,255,255,0.2)",
+          }}
+        >
+          <input type="text" placeholder="Search apple.com" />
+        </div>
+        <ul>
+          {navItems.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   );
 };
 
