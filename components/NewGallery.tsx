51876628107edcb2b3a38785fcf84efb90197b4d
Aung Htet Nyein <aunghtetnyein1999@gmail.com>
2022-09-28 19:55:36 +0630

    Updated animations in gallery'

--- components/NewGallery.tsx
+++ components/NewGallery.tsx
@@ -6,18 +6,52 @@ import styles from "../styles/NewGallery.module.scss";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
 const NewGallery = () => {
+  const [currentSlideState, setCurrentSlideState] = React.useState(3);
   useEffect(() => {
+    const image = document.getElementById("image");
+    const leftClick = document.getElementById("leftClick");
+    const rightClick = document.getElementById("rightClick");
+    const firstLineContainer = document.getElementById("firstLineContainer");
+    const secondLineContainer = document.getElementById("secondLineContainer");
+    const carousel = document.getElementById("carousel");
+    const imageContainer = document.getElementById("imageContainer");
+
+    let currentTranslate = -18.85;
+    let currentSlide = 2;
+    let space = 37.7;
+    const maxSlide = 7;
+
+    const handleLeftClick = () => {
+      if (currentSlide > 1) {
+        currentSlide--;
+        currentTranslate += space;
+      } else {
+        currentSlide = maxSlide - 1;
+        currentTranslate = -space * 2;
+      }
+
+      setCurrentSlideState(currentSlide + 1);
+      imageContainer!.style.transform = `translateX(${currentTranslate}%)`;
+    };
+
+    const handleRightClick = () => {
+      if (currentSlide < maxSlide - 1) {
+        currentSlide++;
+        currentTranslate -= space;
+      } else {
+        currentSlide = 1;
+        currentTranslate = 0;
+      }
+
+      setCurrentSlideState(currentSlide + 1);
+      imageContainer!.style.transform = `translateX(${currentTranslate}%)`;
+    };
+
+    leftClick!.addEventListener("click", handleLeftClick);
+    rightClick!.addEventListener("click", handleRightClick);
+
     document.addEventListener("scroll", () => {
       const scrollY = window.scrollY;
-      const image = document.getElementById("image");
-      const prevImage = document.getElementById("prev");
-      const nextImage = document.getElementById("next");
-      const leftClick = document.getElementById("leftClick");
-      const rightClick = document.getElementById("rightClick");
-      const firstLineContainer = document.getElementById("firstLineContainer");
-      const secondLineContainer = document.getElementById(
-        "secondLineContainer"
-      );
 
       if (scrollY > 1800 && scrollY < 2100) {
         // calculate new width percentage based on scroll position
@@ -28,81 +62,92 @@ const NewGallery = () => {
         image!.style.height = `${100 - newValues}vh`;
         image!.style.marginTop = `${newValues / 2}%`;
         image!.style.borderRadius = "0.8rem";
-
-        prevImage!.style.marginTop = `${newValues / 2}%`;
-        prevImage!.style.width = `${100 - newValues}vw`;
-        prevImage!.style.height = `${100 - newValues}vh`;
-        nextImage!.style.marginTop = `${newValues / 2}%`;
-        nextImage!.style.width = `${100 - newValues}vw`;
-        nextImage!.style.height = `${100 - newValues}vh`;
-
-        leftClick!.style.top = `${newValues + 20}%`;
-        rightClick!.style.top = `${newValues + 20}%`;
       }
 
       if (scrollY > 2000) {
-        prevImage!.style.display = "block";
-        prevImage!.style.opacity = (scrollY - 2000) / 100 + "";
-        nextImage!.style.display = "block";
-        nextImage!.style.opacity = (scrollY - 2000) / 100 + "";
-        leftClick!.style.opacity = (scrollY - 2000) / 100 + "";
-        rightClick!.style.opacity = (scrollY - 2000) / 100 + "";
+        image!.style.opacity = `${1 - (scrollY - 2000) / 80}`;
       } else {
-        prevImage!.style.display = "none";
-        nextImage!.style.display = "none";
+        image!.style.opacity = `1`;
       }
 
-      if (scrollY > 2080) {
-        firstLineContainer!.style.opacity = (scrollY - 2080) / 100 + "";
-        secondLineContainer!.style.opacity = (scrollY - 2080) / 100 + "";
+      if (scrollY > 2050) {
+        carousel!.style.opacity = (scrollY - 2050) / 70 + "";
+        firstLineContainer!.style.opacity = (scrollY - 2050) / 70 + "";
+        secondLineContainer!.style.opacity = (scrollY - 2050) / 70 + "";
       }
     });
+
+    return () => {
+      document.removeEventListener("scroll", () => {});
+      leftClick!.removeEventListener("click", handleLeftClick);
+      rightClick!.removeEventListener("click", handleRightClick);
+    };
   }, []);
+
   return (
     <div className={styles.gallery}>
-      <div className={styles.container}>
-        <div id="prev" className={styles.prev}></div>
-        <div id="image" className={styles.active}></div>
-        <div id="next" className={styles.next}></div>
+      <div className={styles.heroGallery}>
+        <div className={styles.container}>
+          <div
+            id="image"
+            className={styles.active}
+            style={{
+              background: `url("/images/image-${currentSlideState}.jpeg") center/cover no-repeat`,
+            }}
+          ></div>
+        </div>
 
-        <ChevronLeftIcon id={"leftClick"} className={styles.leftIcon} />
-        <ChevronRightIcon id={"rightClick"} className={styles.rightIcon} />
-      </div>
-
-      <div id="firstLineContainer" className={styles.firstLineContainer}>
-        <div className={styles.photobanner}>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
-            (item, index) => (
+        <div id="carousel" className={styles.carousel}>
+          <div id="imageContainer" className={styles.imageContainer}>
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
               // eslint-disable-next-line @next/next/no-img-element
               <img
+                id={`image${item}`}
                 key={index}
-                src={`/images/image-${item > 8 ? item - 8 : item}.jpeg`}
-                alt={`image-${item > 8 ? item - 8 : item}`}
+                src={`/images/image-${item}.jpeg`}
+                alt={`image-${item}`}
               />
-            )
-          )}
+            ))}
+          </div>
+          <ChevronLeftIcon id={"leftClick"} className={styles.leftIcon} />
+          <ChevronRightIcon id={"rightClick"} className={styles.rightIcon} />
         </div>
-      </div>
 
-      <div id="secondLineContainer" className={styles.secondLineContainer}>
-        <div className={styles.photobanner}>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
-            (item, index) => (
-              // eslint-disable-next-line @next/next/no-img-element
-              <img
-                key={index}
-                src={`/images/image-${item > 8 ? item - 8 : item}.jpeg`}
-                alt={`image-${item > 8 ? item - 8 : item}`}
-              />
-            )
-          )}
+        <div id="firstLineContainer" className={styles.firstLineContainer}>
+          <div className={styles.photobanner}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+              (item, index) => (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  key={index}
+                  src={`/images/image-${item > 8 ? item - 8 : item}.jpeg`}
+                  alt={`image-${item > 8 ? item - 8 : item}`}
+                />
+              )
+            )}
+          </div>
         </div>
-      </div>
 
-      <div className={styles.button}>
-        <a href="https://tv.apple.com/channel/tvs.sbd.4000?itscg=10000&itsct=atv-0-tv_ovp-prc_full-apl-avl-200306">
-          See full lineup
-        </a>
+        <div id="secondLineContainer" className={styles.secondLineContainer}>
+          <div className={styles.photobanner}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+              (item, index) => (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  key={index}
+                  src={`/images/image-${item > 8 ? item - 8 : item}.jpeg`}
+                  alt={`image-${item > 8 ? item - 8 : item}`}
+                />
+              )
+            )}
+          </div>
+        </div>
+
+        <div className={styles.button}>
+          <a href="https://tv.apple.com/channel/tvs.sbd.4000?itscg=10000&itsct=atv-0-tv_ovp-prc_full-apl-avl-200306">
+            See full lineup
+          </a>
+        </div>
       </div>
     </div>
   );
