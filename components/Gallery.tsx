e577f2c53f598ff51fdfe677b588c49040ec5770
Aung Htet Nyein <aunghtetnyein1999@gmail.com>
2022-09-30 21:12:09 +0630

    Modified in progress

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -11,15 +11,29 @@ const NewGallery = () => {
 
   useEffect(() => {
     const imageContainer = document.getElementById("imageContainer");
-    // const activeImage = document.getElementById("activeImage");
+    const activeImage = document.getElementById("activeImage");
     const leftClick = document.getElementById("leftClick");
     const rightClick = document.getElementById("rightClick");
+
+    const image1 = document.getElementById("image1");
+    const image2 = document.getElementById("image2");
+    const image3 = document.getElementById("image3");
+    const image4 = document.getElementById("image4");
+    const image5 = document.getElementById("image5");
+    const image6 = document.getElementById("image6");
+    const image7 = document.getElementById("image7");
+    const image8 = document.getElementById("image8");
 
     let currentTranslate = 0;
     let translateAmount = 50;
     let translateAmountForFirstAndLast = 25.5;
     let currentSlide = currentSlideState;
     let maxSlide = 8;
+
+    const setNewImageSize = (image: HTMLElement, values: number) => {
+      image!.style.width = `${100 - values}vw`;
+      image!.style.height = `${100 - values}vh`;
+    };
 
     const handleLeftClick = () => {
       if (currentSlide > 1) {
@@ -65,8 +79,44 @@ const NewGallery = () => {
       imageContainer!.style.transform = `translateX(${currentTranslate}vw)`;
     };
 
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+
+      if (scrollY > 1800 && scrollY < 2350) {
+        // calculate new width percentage based on scroll position
+        const newValues = (scrollY - 1800) / 11;
+
+        // set new width and height for active image
+        setNewImageSize(activeImage!, newValues);
+        activeImage!.style.top = `${newValues / 0.65}vh`;
+
+        // set new width and height for other images
+        image1 && setNewImageSize(image1, newValues);
+        image2 && setNewImageSize(image2, newValues);
+        image3 && setNewImageSize(image3, newValues);
+        image4 && setNewImageSize(image4, newValues);
+        image5 && setNewImageSize(image5, newValues);
+        image6 && setNewImageSize(image6, newValues);
+        image7 && setNewImageSize(image7, newValues);
+        image8 && setNewImageSize(image8, newValues);
+
+        imageContainer!.style.transform = `translateX(${currentTranslate}vw)`;
+      }
+
+      if (scrollY >= 2350) {
+        activeImage!.style.position = "block";
+        // activeImage!.style.transform = "translateX(-50%)";
+        imageContainer!.style.overflowX = "visible";
+      } else {
+        activeImage!.style.position = "absolute";
+        // activeImage!.style.transform = "translateX(0)";
+        imageContainer!.style.overflowX = "hidden";
+      }
+    };
+
     leftClick?.addEventListener("click", handleLeftClick);
     rightClick?.addEventListener("click", handleRightClick);
+    document.addEventListener("scroll", handleScroll);
 
     return () => {
       leftClick?.removeEventListener("click", handleLeftClick);
@@ -78,11 +128,12 @@ const NewGallery = () => {
   return (
     <div className={styles.gallery}>
       <div className={styles.heroContainer}>
+        <p>{currentSlideState}</p>
         <div id="imageContainer" className={styles.imageContainer}>
           {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
             // eslint-disable-next-line @next/next/no-img-element
             <div
-              id={currentSlideState === item ? "activeImage" : ""}
+              id={currentSlideState === item ? "activeImage" : `image${item}`}
               className={item === currentSlideState ? styles.active : ""}
               key={index}
               style={{
